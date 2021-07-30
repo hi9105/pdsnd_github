91ffac356ddfd83f9372685a38c65c0ee5670c76
harnalhi <hiral.harnal@ista.com>
2021-07-30 16:30:54 +0200

    Added note in Readme file and in Python script

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -6,6 +6,10 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+
+# Second change for Refactoring branch
+
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
